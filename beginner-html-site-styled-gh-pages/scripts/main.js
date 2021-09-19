88824c6cc4b983a5fb19d871e4ff09ca548e6f09
unserxe <89406318+unserxe@users.noreply.github.com>
2021-09-19 20:29:31 +0800

    09190829test

---
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -0,0 +1,22 @@
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName || myName === null) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    }
+  }
+
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+  }
+
+  myButton.onclick = function() {
+    setUserName();
+  }
